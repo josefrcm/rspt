ef3b90d2429d1caf27253749713ec3a4a34cd2a9
José Franco Campos <josefrancocampos@gmail.com2>
2019-01-08 10:58:50 +0900

    Rename the project.

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 set RUST_BACKTRACE=1
 cargo build --release
 del target\release\render.png
-target\release\rusty_ray -s 100 -i data\scene.json -c data\camera.json -o target\release\render.png
+target\release\rspt -s 100 -i data\scene.json -c data\camera.json -o target\release\render.png
--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 export RUST_BACKTRACE=1
 cargo build --release
 rm target/release/render.png
-time target/release/rusty_ray -s 100 -i data/scene.json -c data/camera.json -o target/release/render.hpngdr
+time target/release/rspt -s 100 -i data/scene.json -c data/camera.json -o target/release/render.hpngdr
