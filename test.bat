f37385ce8e0a794d6e774e142ffc3bf66e859607
José Franco Campos <josefrancocampos@gmail.com>
2019-01-08 21:56:08 +0900

    Fixed a minor bug regarding the output path.

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 set RUST_BACKTRACE=1
 cargo build --release
-del target\release\render.png
-target\release\rspt -s 100 -i data\scene.json -c data\camera.json -o target\release\render.png
+del render.png
+target\release\rspt -s 100 -i data\scene.json -c data\camera.json -o render.png
--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 export RUST_BACKTRACE=1
 cargo build --release
-rm target/release/render.png
-time target/release/rspt -s 100 -i data/scene.json -c data/camera.json -o target/release/render.hpngdr
+rm render.png
+target/release/rspt -s 100 -i data/scene.json -c data/camera.json -o render.png
